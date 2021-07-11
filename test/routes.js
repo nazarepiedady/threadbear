a14ce9bd4c720639b5e703e5714d359b1d3c239c
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-07-11 11:25:13 +0100

    create shouldHaveMessage helper function
    to test wether a request to an address returns a
    message we expect

--- test/routes.js
+++ test/routes.js
@@ -20,3 +20,29 @@ const shouldBeOk = (method, path, done) => {
     )
     .end()
 }
+
+
+const shouldHaveMessage = (method, path, message, done) => {
+  http
+    .request(
+      {
+        host: process.env.HOST,
+        port: process.env.PORT,
+        method,
+        path,
+      },
+      response => {
+        let data = ''
+
+        response.on('data', chunk => {
+          data += chunk
+        })
+
+        response.on('end', () => {
+          assert.equal(message, data)
+          done()
+        })
+      },
+    )
+    .end()
+}
