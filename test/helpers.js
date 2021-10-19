ccb702b7beff84753a99c15492a75b039ae01c04
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-10-19 19:36:58 +0100

    chore: move the helpers code from test/route.js to test/helpers.js

---
+++ test/helpers.js
@@ -0,0 +1,48 @@
+const http = require('http')
+const assert = require('assert')
+
+require('dotenv').config()
+
+
+const shouldBeOk = (method, path, done) => {
+  http
+    .request(
+      {
+        host: process.env.HOST,
+        port: process.env.PORT,
+        method,
+        path,
+      },
+      response => {
+        assert.equal(200, response.statusCode);
+        done()
+      },
+    )
+    .end()
+}
+
+
+const shouldHaveMessage = (method, path, message, done) => {
+  http
+    .request(
+      {
+        host: process.env.HOST,
+        port: process.env.PORT,
+        method,
+        path,
+      },
+      response => {
+        let data = ''
+
+        response.on('data', chunk => {
+          data += chunk
+        })
+
+        response.on('end', () => {
+          assert.equal(message, data)
+          done()
+        })
+      },
+    )
+    .end()
+}
