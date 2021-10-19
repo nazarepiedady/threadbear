aeb6c3afac6496e95972305e19230dcfc47306b2
Nazaré da Piedade <nazarepiedady@gmail.com>
2021-10-19 20:14:41 +0100

    chore: create new test helper function named request

--- test/helpers.js
+++ test/helpers.js
@@ -1,3 +1,11 @@
+const http = require('http')
+const assert = require('assert')
+const querystring = require('querystring')
+
+
+require('dotenv').config()
+
+
 const shouldBeOk = (method, path, done) => {
   http
     .request(
@@ -42,7 +50,56 @@ const shouldHaveMessage = (method, path, message, done) => {
 }
 
 
+const request = (method, path, callback, options) => {
+  let data = ''
+  const methods = ['POST', 'PUT', 'PATCH']
+
+  if (
+    options.data &&
+    methods.indexOf(method) > -1
+  ) {
+    data = querystring.stringify(options.data)
+  }
+
+  const parameters = Object.assign(
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(data)
+      },
+      host: process.env.HOST,
+      port: process.env.PORT,
+      method,
+      path
+    },
+    options
+  )
+
+  const request = http.request(
+    parameters,
+    response => {
+      let buffer = ''
+
+      response.on('data', data => {
+        buffer += data
+      })
+
+      response.on('end', () => {
+        callback(buffer, response)
+      })
+    }
+  )
+
+  if (data) {
+    request.write(data)
+  }
+
+  request.end()
+}
+
+
 module.exports = {
+  request,
   shouldBeOk,
   shouldHaveMessage
 }
