99ba2880818a060b970c14a5cbb470bcd3679360
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-01-24 20:15:59 +0100

    feat: take the user location based on ip and set currency to app

--- app/Middleware/CurrencySelector.js
+++ app/Middleware/CurrencySelector.js
@@ -3,6 +3,15 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+// npm install --save geoip-lite
+// npm install --save currency-map-country
+const geoip = use('geoip-lite')
+const {
+  getCountry,
+  getCountryByAbbreviation,
+  getCurrency
+} = use('current-map-country')
+
 class CurrencySelector {
   /**
    * @param {object} ctx
@@ -11,6 +20,25 @@ class CurrencySelector {
    */
   async handle ({ request }, next) {
     // call next to advance the request
+    const ip = request.ip()
+    const reference = geoip.lookup(ip)
+
+    let currency = null
+
+    if (!reference) {
+      request.country = 'Unknown'
+      request.currency = 'US'
+
+      currency = getCurrency('USD')
+    } else {
+      request.country = reference.country
+      const name = getCountryByAbbreviation(request.country)
+      const country = getCountry(name);
+      currency = getCurrency(country.cur)
+    }
+
+    request.currencyFormat = currency.symbolFormat
+
     await next()
   }
 }
