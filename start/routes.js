37c7ea8513372dad9e9a8b87f832653f358dc4b2
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-01-24 19:16:00 +0100

    chore: let simple routes and remove route handlers from routes.js

--- start/routes.js
+++ start/routes.js
@@ -20,182 +20,38 @@ Route.get('/', ({ view }) => {
   return view.render('page/home')
 })
 
-// register routes
-Route.get('/register', ({ view }) => {
-  // show registration form
-  return view.render('user/register')
-})
 
-Route.post('/register', ({ request, response }) => {
-  // create new customer profile
-  response.json(request.all())
-})
+Route.get('/login', 'CustomerController.showLogin')
 
 
-// login routes
-Route.get('/login', ({ view }) => {
-  // show login form
-  return view.render('user/login')
-})
-
-Route.post('/login', () => {
-  // create new customer session
-  return 'POST /login'
-})
+Route.post('/login', 'CustomerController.login')
 
 
-// logout routes
-Route.put('/logout', () => {
-  // expire current customer session
-  return 'PUT /logout'
-})
+Route.put('/logout', 'CustomerController.logout')
 
 
-// forgot-password routes
-Route.get('/forgot-password', () => {
-  // show forgot password form
-  return 'GET /forgot-password'
-})
-
-Route.post('/forgot-password', () => {
-  // create new password reset token and send e-mail
-  return 'POST /forgot-password'
-})
+Route.get('/register', 'CustomerController.showRegister')
 
 
-// reset-password routes
-Route.get('/reset-password/:token', ({ params }) => {
-  // show forgot password form
-  return 'GET /reset-password ' + params.token
-})
-
-Route.patch('/reset-password/:token', ({ request, params }) => {
-  // create new password reset token and send e-mail
-  //return 'POST /reset-password ' + params.token
-  return JSON.stringify(request.all())
-})
+Route.post('/register', 'CustomerController.register')
 
 
-const redirects = {
-  assertchris: 'christopher',
-  thetutlage: 'harminder'
-}
-
-// customer routes
-Route.get('/:customer', ({ response, view, params }) => {
-  // show customer profile
-  const redirect = redirects[params.customer]
-
-  if (redirect)
-    return response.redirect('/' + redirect)
-
-  return 'Welcome ' + params.customer
-})
-
-Route.put('/:customer', ({ params }) => {
-  // update customer profile
-  return 'PUT /:customer ' + params.customer
-})
-
-Route.delete('/:customer', ({ params }) => {
-  // delete customer profile
-  return 'DELETE /:customer ' + params.customer
-})
+Route.get('/forgot-password', 'CustomerController.showForgotPassword')
 
 
-/*
-// npm install --save js2xmlparser
-const parser = use('js2xmlparser')
-const Response = use('Response')
-
-Response.macro('xml', function(data, root='root') {
-  this.type('application/xml')
-  this.send(parser.parse(root, data))
-})
-
-Response.macro('for', function(params, handlers) {
-  if (params.format === '.xml') {
-    const handler = handlers.xml
-    const data = handler()
-
-    return this.xml(...data)
-  }
-
-  if (params.format === '.json') {
-    const handler = handlers.json
-    const data = handler()
-
-    return this.json(...data)
-  }
-
-  return (handlers.default || function() {})()
-})
-
-Route.get('/:customer/products', ({ params, response }) => {
-  const products = [
-    { price: 4.99, title: 'Teddy Bear' }
-  ]
-
-  return response.for(params, {
-    xml: () => [
-      {
-        product: products.map(product => ({
-          '@': { price: product.price },
-          '#': product.title
-        }))
-      },
-      'products'
-    ],
-    json: () => [
-      {
-        products
-      }
-    ],
-    default: () => '...render normal view'
-  })
-}).formats(['xml', 'json'])
-*/
-
-// customer products routes
-
-Route.get('/:customer/products', ({ params }) => {
-  // show customer's products
-  return 'GET /:customer/products ' + params.customer
-})
-
-Route.post('/:customer/products', ({ params }) => {
-  // create a new product
-  return 'POST /:customer/products ' + params.customer
-})
+Route.post('/forgot-password', 'CustomerController.forgotPassword')
 
 
-// customer product routes
-Route.get('/:customer/:product', ({ params }) => {
-  // show customer product profile
-  return (
-    'GET /:customer/:product ' +
-    params.customer +
-    ' ' +
-    params.product
-  )
-})
+Route.get('/reset-password/:token', 'CustomerController.showResetPassword')
 
-Route.put('/:customer/:product', ({ params }) => {
-  // update customer product profile
-  return (
-    'PUT /:customer/:product ' +
-    params.customer +
-    ' ' +
-    params.product
-  )
-})
 
-Route.delete('/:customer/:product', ({ params }) => {
-  // delete customer product profile
-  return (
-    'DELETE /:customer/:product ' +
-    params.customer +
-    ' ' +
-    params.product
-  )
-})
+Route.patch('/reset-password/:token', 'CustomerController.resetPassword')
+
+
+Route.get('/:customer', 'CustomerController.showProfile').as('profile')
+
+
+Route.put('/:customer', 'CustomerController.updateProfile')
+
+
+Route.delete('/:customer', 'CustomerController.deleteProfile')
