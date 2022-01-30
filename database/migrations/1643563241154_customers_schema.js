4216fc142a5de40b92b2d13d3b619d4cfd5a5c90
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-01-30 18:24:07 +0100

    chore: write the customers migration logic
    which will create a table with five columns that correspond each
    customers attribute such as first_name, last_name, email...

--- database/migrations/1643563241154_customers_schema.js
+++ database/migrations/1643563241154_customers_schema.js
@@ -7,6 +7,11 @@ class CustomersSchema extends Schema {
   up () {
     this.create('customers', (table) => {
       table.increments()
+      table.string('first_name')
+      table.string('last_name')
+      table.string('email').unique()
+      table.string('password')
+      table.string('nickname').unique()
       table.timestamps()
     })
   }
