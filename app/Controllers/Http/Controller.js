3e574d70cd23cf6fb334ee7e6cc50e3f9ca06212
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-03 16:16:43 +0100

    chore: abstract `validate` logic into base controller

--- app/Controllers/Http/Controller.js
+++ app/Controllers/Http/Controller.js
@@ -1,8 +1,21 @@
 'use strict'
 
+const { validateAll } = use('Validator')
+
 class Controller {
   showRequestParameters({ request, response }) {
     response.json(request.all())
+  }
+
+  async validate({ request, response, session, rules }) {
+    const validation = await validateAll(request.all(), rules)
+
+    if (validation.fails()) {
+      session
+        .withErrors(validation.messages())
+        .flashAll()
+      return response.redirect('back')
+    }
   }
 }
 
