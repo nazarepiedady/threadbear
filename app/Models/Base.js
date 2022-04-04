94d3ec1777d925d1d1ce148880b9ce08caa4c249
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-04 17:05:11 +0100

    chore: create a `title case` method inside `base` model

--- app/Models/Base.js
+++ app/Models/Base.js
@@ -2,8 +2,12 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const startCase = use('lodash/startCase')
 
 class Base extends Model {
+  titleCase(...params) {
+    return startCase(...params)
+  }
 }
 
 module.exports = Base
