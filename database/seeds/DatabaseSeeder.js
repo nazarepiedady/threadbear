46504166926ad0e17a0e7e57bd9629fb9c13b811
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-11 13:14:05 +0100

    refactor: improve `database seeder` to fill the database with some informations

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -10,26 +10,30 @@
 |
 */
 
+const Hash = use('Hash')
+const moment = use('moment')
 const Factory = use('Factory')
 const Database = use('Database')
-const Hash = use('Hash')
 
 
 class DatabaseSeeder {
   async run() {
-    const created_at = Database.raw('CURRENT_TIME')
+    const timestamps = {
+      created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+      updated_at: moment().format('YYYY-MM-DD HH:mm:ss')
+    }
 
     // insert redirects
     await Database.insert({
       from: 'assertchris',
       to: 'christopher',
-      created_at
+      ...timestamps
     }).into('redirects')
 
     await Database.insert({
       from: 'thetutlage',
       to: 'harminder',
-      created_at
+      ...timestamps
     }).into('redirects')
 
 
@@ -40,26 +44,53 @@ class DatabaseSeeder {
       email: 'virk.officials@gmail.com',
       password: await Hash.make('harminder123'),
       nickname: 'harminder',
-      created_at
+      ...timestamps
     }).into('customers')
 
-    const ids = await Database.insert({
+    await Database.insert({
       first_name: 'Christopher',
       last_name: 'Pitt',
       email: 'cgpitt@gmail.com',
       password: await Hash.make('christopher123'),
       nickname: 'christopher',
-      created_at
+      ...timestamps
     }).into('customers')
 
+    const customerIds = await this.ids('customers')
 
     // insert products
     await Database.insert({
       name: 'Soft Teddy',
       price: 499,
       customer_id: ids[0],
-      created_at
+      ...timestamps
     }).into('products')
+
+    const productsIds = await this.ids('products')
+
+    await Database.insert([{
+      buyer_id: customerIds[0],
+      seller_id: customerIds[1],
+      status: 'pending',
+      ...timestamps
+    }]).into('orders')
+
+    const orderIds = await this.ids('orders')
+
+    await Database.insert([{
+      order_id: orderIds[0],
+      product_id: productsIds[0],
+      quantity: 2,
+      price: 499,
+      ...timestamps
+    }]).into('order_items')
+  }
+
+  async ids(table) {
+    return await Database.select('id')
+      .from(table)
+      .orderBy('id', 'asc')
+      .map(next => next.id)
   }
 }
 
