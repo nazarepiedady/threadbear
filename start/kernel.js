24d08c2e103358b514ebc037062a3f88aabd4547
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-17 10:14:07 +0100

    chore: register the custom `auth` middleware to namedMiddleware
    set a prefix of `adonis` to the default `auth` middleware that
    came with adonis to use the custom `auth` middleware created.

--- start/kernel.js
+++ start/kernel.js
@@ -39,7 +39,8 @@ const globalMiddleware = [
 |
 */
 const namedMiddleware = {
-  auth: 'Adonis/Middleware/Auth',
+  auth: 'App/Middleware/Auth'
+  adonis_auth: 'Adonis/Middleware/Auth',
   guest: 'Adonis/Middleware/AllowGuestOnly'
 }
 
