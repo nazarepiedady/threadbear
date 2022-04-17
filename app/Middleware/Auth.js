0e54d3da0ac0942262556f94860ff1c1b784b75f
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-04-17 09:31:23 +0100

    feat: implement the logic for `auth` middleware
    it checks if current user as a session and redirect him to `login`
    route he does not have a session, then also checks if this user is
    registered if not it redirect the user to `login` route.

--- app/Middleware/Auth.js
+++ app/Middleware/Auth.js
@@ -3,14 +3,32 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const key = 'customer'
+const redirectTo = 'login'
+const Customer = use('App/Models/Customer')
+
 class Auth {
   /**
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Function} next
    */
-  async handle ({ request }, next) {
+  async handle ({ request, session, response }, next) {
     // call next to advance the request
+    const id = session.get(key)
+
+    if (!id) {
+      return response.route(redirectTo)
+    }
+
+    const customer = await Customer.find(id)
+
+    if (!customer) {
+      return response.route(redirectTo)
+    }
+
+    request.customer = customer
+
     await next()
   }
 }
