54c7fa8768d898019f0a1702eaeb233e6decf5d9
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-06-05 13:18:13 +0100

    chore: add `webpack.config.js` to root directory

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = [
+  {
+    entry: path.resolve(__dirname, 'resources/js/app.js'),
+    output: {
+      path: path.resolve(__dirname, 'public/js/'),
+      filename: 'app.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+  },
+  
+/*  {
+    entry: path.resolve(__dirname, 'resources/sass/app.scss'),
+    output: {
+      path: path.resolve(__dirname, 'public/js/'),
+      filename: 'app.css'
+    },
+    module: {
+      rules: [
+        { test: /\.scss$/, use: 'sass-loader' }
+      ]
+    }
+  }*/
+];
